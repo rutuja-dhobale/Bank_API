35b49f421dafdffb8a22de6ea62b88a6aa60b044
rutujadhobale <rutujadhobale24@gmail.com>
2024-11-22 09:30:11 +0530

    first commit

---
+++ src/main/java/com/tka/SringBootHibernate/SringBootHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.tka.SringBootHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SringBootHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SringBootHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tka/SringBootHibernate/SringBootHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tka.SringBootHibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SringBootHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
